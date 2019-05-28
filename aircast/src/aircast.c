06abcdf888c224df6ea2883480e155a468e5d3c7
philippe44 <philippe_44@outlook.com>
2019-05-28 16:48:49 -0700

    0.2.10.2

--- aircast/src/aircast.c
+++ aircast/src/aircast.c
@@ -36,7 +36,7 @@
 #include "config_cast.h"
 #include "sslsym.h"
 
-#define VERSION "v0.2.10.1"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v0.2.10.2"" ("__DATE__" @ "__TIME__")"
 
 #define DISCOVERY_TIME 20
 

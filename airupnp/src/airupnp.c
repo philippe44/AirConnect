cb731dce032268ea93e5a7e6d5b3dc73c95b6c88
Philippe G <philippe44@users.noreply.github.com>
2020-04-07 14:46:22 -0700

    0.2.24.7

--- airupnp/src/airupnp.c
+++ airupnp/src/airupnp.c
@@ -38,7 +38,7 @@
 #include "log_util.h"
 #include "sslsym.h"
 
-#define VERSION "v0.2.24.6"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v0.2.24.7"" ("__DATE__" @ "__TIME__")"
 
 #define	AV_TRANSPORT 			"urn:schemas-upnp-org:service:AVTransport"
 #define	RENDERING_CTRL 			"urn:schemas-upnp-org:service:RenderingControl"
@@ -1328,16 +1328,6 @@ int main(int argc, char *argv[])
 {
 	int i;
 	char resp[20] = "";
-
-	char cmd[]="18=jdk<hkjdqshkjdhkjhqskd,4=jdkqsljldkqjdlkqs";
-	int gpio;
-	char *p, type[4];
-
-	p = cmd;
-	do {
-		sscanf(p, "%d=%[^,]", &gpio, type);
-		p = strchr(p, ',');
-	} while (p++);
 
 	signal(SIGINT, sighandler);
 	signal(SIGTERM, sighandler);

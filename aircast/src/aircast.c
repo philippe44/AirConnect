fe7b1866c3b0d982805e40dcf247a2bafcbd5186
Philippe G <philippe44@users.noreply.github.com>
2020-12-15 16:16:33 -0800

    0.2.42.1

--- aircast/src/aircast.c
+++ aircast/src/aircast.c
@@ -36,7 +36,7 @@
 #include "config_cast.h"
 #include "sslsym.h"
 
-#define VERSION "v0.2.42.0"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v0.2.42.1"" ("__DATE__" @ "__TIME__")"
 
 #define DISCOVERY_TIME 	20
 #define MEDIA_VOLUME	0.5

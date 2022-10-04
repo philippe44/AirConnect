521a609f8787d79c36ef41803517736ed243d25e
philippe44 <philippe44@users.noreply.github.com>
2022-10-03 18:35:38 -0700

    add other platforms

--- airupnp/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-make -f Makefile.arm $1
-make -f Makefile.aarch64 $1
-make -f Makefile.x86 $1
-make -f Makefile.x86_64 $1
-#make -f Makefile.i86pc-solaris $1
-#make -f Makefile.bsd-x64 $1
-#make -f Makefile.osx $1
---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+list="x86_64-linux-gnu-gcc x86-linux-gnu-gcc arm-linux-gnueabi-gcc aarch64-linux-gnu-gcc sparc64-linux-gnu-gcc mips-linux-gnu-gcc powerpc-linux-gnu-gcc"
+declare -A alias=( [x86-linux-gnu-gcc]=i686-linux-gnu-gcc )
+declare -A cppflags=( [mips-linux-gnu-gcc]="-march=mips32" [powerpc-linux-gnu-gcc]="-m32")
+declare -a compilers
+
+IFS= read -ra candidates <<< "$list"
+
+# do we have "clean" somewhere in parameters (assuming no compiler has "clean" in it...
+if [[ $@[*]} =~ clean ]]; then
+	clean="clean"
+fi	
+
+# first select platforms/compilers
+for cc in ${candidates[@]}
+do
+	# check compiler first
+	if ! command -v ${alias[$cc]:-$cc} &> /dev/null; then
+		continue
+	fi
+	
+	if [[ $# == 0 || ($# == 1 && -n $clean) ]]; then
+		compilers+=($cc)
+		continue
+	fi
+
+	for arg in $@
+	do
+		if [[ $cc =~ $arg ]]; then 
+			compilers+=($cc)
+		fi
+	done
+done
+
+# then do the work
+for cc in ${compilers[@]}
+do
+	IFS=- read -r platform host dummy <<< $cc
+
+	make CC=${alias[$cc]:-$cc} PLATFORM=$platform $clean
+	if [[ -n $clean ]]; then
+		continue
+	fi
+done

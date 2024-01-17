47b0c5ef53c5be1a5ea3154bbe5e8e2760b1729e
philippe44 <philippe44@users.noreply.github.com>
2024-01-16 18:33:38 -0800

    1.8.0

--- aircast/src/aircast.h
+++ aircast/src/aircast.h
@@ -18,7 +18,7 @@
 #include "raop_server.h"
 #include "cast_util.h"
 
-#define VERSION "v1.7.1"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v1.8.0"" ("__DATE__" @ "__TIME__")"
 
 /*----------------------------------------------------------------------------*/
 /* typedefs */
--- airupnp/src/airupnp.h
+++ airupnp/src/airupnp.h
@@ -21,7 +21,7 @@
 #include "cross_util.h"
 #include "metadata.h"
 
-#define VERSION "v1.7.1"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v1.8.0"" ("__DATE__" @ "__TIME__")"
 
 /*----------------------------------------------------------------------------*/
 /* typedefs */
